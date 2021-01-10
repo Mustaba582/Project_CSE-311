1e18086feaaab223c9b4e81bff8aceecb3706b0a
1911076642-Mustaba Mubasweer <mustaba.mubasweer@northsouth.edu>
2021-01-10 11:50:10 +0600

    create account connection created

--- PHP/create_new_account.php
+++ PHP/create_new_account.php
@@ -1,8 +1,6 @@
 <?php
 include "connection.php";
 
-
-
 ?>
 
 
@@ -21,41 +19,61 @@ include "connection.php";
   </header>
 
   <section class="section1">
+    <div>
+      <form class="my-form" action="" method="post">
+        <div class="form-group">
+          <label>User ID: </label>
+          <input type="number" name="user_id" required="">
 
-    <form class="my-form">
-      <div class="form-group">
-        <label>User ID: </label>
-        <input type="number" name="user_id" required="">
-      </div>
-      <div class="form-group">
-        <label>Password: </label>
-        <input type="password" name="password" required="">
-      </div>
-      <div class="form-group">
-        <label>Name: </label>
-        <input type="text" name="Name" required="">
-      </div>
-      <div class="form-group">
-        <label>Contact no: </label>
-        <input type="number" name="contact_no" required="">
-      </div>
-      <div class="form-group">
-        <label>Address: </label>
-        <input type="text" name="address" required="">
-      </div>
-      <br><br>
-      <div>
-        <input class="button" type="submit" name="submit" value="Create Account">
-      </div>
-    </form>
+          <label>Password: </label>
+          <input type="password" name="password" required="">
+
+          <label>Name: </label>
+          <input type="text" name="Name" required="">
+
+          <label>Contact no: </label>
+          <input type="number" name="contact_no" required="">
+
+          <label>Address: </label>
+          <input type="text" name="address" required="">
+          <br><br>
+          <input class="button" type="submit" name="submit" value="Create Account">
+        </div>
+      </form>
+    </div>
   </section>
 
   <?php
+
   if (isset($_POST['submit'])) {
-    mysqli_query($connection, "INSERT INTO `member_info` VALUES ('$_POST[user_id]', '$_POST[password]', '$_POST[Name]', '$_POST[contact_no]', '$_POST[address]');");
-    echo "Pushed";
+    $count = 0;
+    $sql = "SELECT user_id from `member_info`";
+    $res = mysqli_query($connection, $sql);
+
+    while ($row = mysqli_fetch_assoc($res)) {
+      if ($row['user_id'] == $_POST['user_id']) {
+        $count = $count + 1;
+      }
+    }
+    if ($count == 0) {
+      mysqli_query($connection, "INSERT INTO `member_info` VALUES('$_POST[user_id]', '$_POST[password]', '$_POST[Name]', '$_POST[contact_no]', '$_POST[address]');");
+  ?>
+      <script type="text/javascript">
+        alert("Registration successful");
+      </script>
+    <?php
+    } else {
+
+    ?>
+      <script type="text/javascript">
+        alert("The username already exist.");
+      </script>
+  <?php
+
+    }
   }
   ?>
+
 
 </body>
 
