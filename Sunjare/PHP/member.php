c92d21edc2252064b2ad861996224cc4cb5bc3e8
1911076642-Mustaba Mubasweer <mustaba.mubasweer@northsouth.edu>
2021-01-17 17:32:39 +0600

    Login and Member page prblem resolved

--- Sunjare/PHP/member.php
+++ Sunjare/PHP/member.php
@@ -2,15 +2,14 @@
 include "connection.php";
 //Taken
 session_start();
-if(isset($_SESSION['login'])) {
-   if($_SESSION['login'] != true) {
+if (isset($_SESSION['login'])) {
+   if ($_SESSION['login'] != true) {
       header("Location: ./index.php");
    }
 } else {
    header("Location: ./index.php");
 }
-if(isset($_POST['logout'])) 
-{
+if (isset($_POST['logout'])) {
    unset($_SESSION['login'], $_SESSION['name'], $_SESSION['user_id']);
    header("Location: ./index.php");
 }
@@ -32,7 +31,7 @@ if(isset($_POST['logout']))
    <div class="user_info_box">
 
       <div class="top">
-        
+
          <p>Name: <?php echo isset($_SESSION['name']) ? $_SESSION['name'] : ""; ?> <br><br> User ID: <?php echo isset($_SESSION['user_id']) ? $_SESSION['user_id'] : ""; ?> <br><br> Membership Validity: <?php echo isset($_SESSION['mem_exp']) ? $_SESSION['mem_exp'] : ""; ?> <br><br></p>
 
       </div>
@@ -87,16 +86,16 @@ if(isset($_POST['logout']))
 
       <div>
 
-         <form class="form" action = "" method="post" style="width: 80%;">
+         <form class="form" action="" method="post" style="width: 80%;">
             <h1 class="hi2"> Find Books</h1>
             <label>Book Name or ISBN:</label> <br>
-            <input type="text" name="book_isbn" />
-            <input class = "SE"  type="submit" name="search" value="Search"> 
-            <input class = "po"  type="submit" name="add_to_list" value="Add to list">
-            
+            <input type="text" name="book_isbn">
+            <input class="SE" type="submit" name="search" value="Search">
+            <input class="po" type="submit" name="add_to_list" value="Add to list">
+
          </form>
 
-         
+
       </div>
 
    </div>
@@ -105,77 +104,69 @@ if(isset($_POST['logout']))
 
    <?php
 
-         $count = 0;
-         $sql = "SELECT Book_id, Book_name, Genre from `all_book_list`";
-         $res = mysqli_query($connection, $sql);         
-         
-         while ($row = mysqli_fetch_assoc($res)) {
-            if ($row['Book_id'] == $_POST['book_isbn']) {
-              $count = $count + 1;
-              break;
-            }
-          }
+   $count = 0;
+   $sql = "SELECT Book_id, Book_name, Genre from `all_book_list`";
+   $res = mysqli_query($connection, $sql);
+   $bookname = null;
+   while ($row = mysqli_fetch_assoc($res)) {
+      if ($row['Book_id'] == $_POST['book_isbn']) {
+         $count = $count + 1;
+         $bookname = $row['Book_name'];
+         break;
+      }
+   }
 
 
-         if(isset($_POST['search'])) { 
-            
-             if ($count == 1) {
-               
-           ?>
-               <script type="text/javascript">
-                 alert("Book Found!");
-               </script>
-             <?php
-             } else {
-         
-             ?>
-               <script type="text/javascript">
-                 alert("Sorry, this Book is not available");
-               </script>
-           <?php
-         
-             }
-        } 
+   if (isset($_POST['search'])) {
+
+      if ($count == 1) {
+
+   ?>
+         <script type="text/javascript">
+            alert("Book Found!");
+         </script>
+      <?php
+      } else {
+
+      ?>
+         <script type="text/javascript">
+            alert("Sorry, this Book is not available");
+         </script>
+      <?php
+
+      }
+   }
 
 
-        if(isset($_POST['add_to_list'])) {
-          
-          $issue_date = strtotime("");
-          $issue_date_store = date("Y-m-d H:i:s", $issue_date);
+   if (isset($_POST['add_to_list'])) {
 
-          $return_date = strtotime("+3 Month");
-          $return_date_store = date("Y-m-d H:i:s", $return_date);
+      $issue_date = strtotime("today");
+      $issue_date_store = date("Y-m-d h:i:s", $issue_date);
 
-           if ($count == 1) {
-                while ($row = mysqli_fetch_assoc($res)) {
-                      if ($row['Book_id'] == $_POST['book_isbn']) {
-                         $sql2 = "INSERT INTO borrowed_book_list VALUES ($_SESSION[user_id] , $_POST[book_isbn] , $row[Book_name], '$issue_date_store', '$return_date_store')";
-                         $res2 = mysqli_query($connection, $sql2);
-                      }
-                }   
-            
-              
-            
-               
-            ?>
-                <script type="text/javascript">
-                  alert("Book Added!");
-                </script>
-              <?php
-              } 
+      $return_date = strtotime("+3 Month");
+      $return_date_store = date("Y-m-d H:i:s", $return_date);
+      echo $_SESSION['user_id'] . 'And ' . $_POST['book_isbn'] . $bookname . $issue_date_store . $return_date_store;
+      if ($count == 1) {
+         mysqli_query($connection, "INSERT INTO `borrowed_book_list` VALUES ('$_SESSION[user_id]' , '$_POST[book_isbn]' , '$bookname', '$issue_date_store', '$return_date_store')");
 
-              else {
-          
-              ?>
-                <script type="text/javascript">
-                  alert("Sorry, this Book is not available");
-                </script>
-            <?php
-          
-              }
-        }
 
-        ?>
+      ?>
+         <script type="text/javascript">
+            alert("Book Added!");
+         </script>
+      <?php
+      } else {
+
+      ?>
+         <script type="text/javascript">
+            alert("Sorry, this Book is not available");
+         </script>
+   <?php
+
+      }
+   }
+
+   ?>
 
 
 
