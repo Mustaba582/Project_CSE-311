1fa6117514bbb04b3d69d579478a2a5cd675b337
1911076642-Mustaba Mubasweer <mustaba.mubasweer@northsouth.edu>
2021-01-23 16:08:55 +0600

    final file bug fixed and resolved2

--- Final/member.php
+++ Final/member.php
@@ -40,13 +40,13 @@ if (isset($_POST['logout'])) {
 
 
          <p>
-         Name: <?php echo isset($_SESSION['name']) ? $_SESSION['name'] : ""; ?> 
-         <br><br> 
-         User ID: <?php echo isset($_SESSION['user_id']) ? $_SESSION['user_id'] : ""; ?> 
-         <br><br> 
-         Membership Validity: <?php echo isset($_SESSION['mem_exp']) ? $_SESSION['mem_exp'] : ""; ?> 
-         
-         <br><br>
+            Name: <?php echo isset($_SESSION['name']) ? $_SESSION['name'] : ""; ?>
+            <br><br>
+            User ID: <?php echo isset($_SESSION['user_id']) ? $_SESSION['user_id'] : ""; ?>
+            <br><br>
+            Membership Validity: <?php echo isset($_SESSION['mem_exp']) ? $_SESSION['mem_exp'] : ""; ?>
+
+            <br><br>
          </p>
 
 
@@ -70,77 +70,79 @@ if (isset($_POST['logout'])) {
 
    <div class="borrowed_box">
 
-      
-         <h1 class="hi1">Borrowed Books</h1>
 
-         <ol class="borrowed_list">
+      <h1 class="hi1">Borrowed Books</h1>
+
+      <ol class="borrowed_list">
 
 
-            <?php
+         <?php
 
-                  
 
-               
-                  $count_book = 1;      
-                  $sql = "Select Mem_Id , Book_id , Book_Name , Issue_date , Return_date
+
+
+         $count_book = 1;
+         $sql = "Select Mem_Id , Book_id , Book_Name , Issue_date , Return_date
                   from borrowed_book_list";
-                  $result = mysqli_query($connection, $sql);
-
-                  
-                  
-
-                  echo"<br>";
-
-                  
-                  if(mysqli_num_rows($result) > 0){
-                     while($row = mysqli_fetch_assoc($result)){
-                        if($_SESSION['user_id'] == $row['Mem_Id']){
-                        echo  "Serial No:   " .$count_book .", ISBN:  " ." " .$row["Book_id"] ." " .", Name: " ." " .$row["Book_Name"] ." " .", Issue Date:   "  ." " .$row["Issue_date"] ." " ." , Return Date: " ." " .$row["Return_date"];
-                        echo "<br><br>";
-                        $count_book = $count_book + 1;
-                      }
-                     }
-                    }
+         $result = mysqli_query($connection, $sql);
 
 
-            ?>
 
-         </ol>
-         <a href="all_book_list_member.php">
-            <button class="SA">View All books</button>
-         </a>
-      
+
+         echo "<br>";
+
+
+         if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+               if ($_SESSION['user_id'] == $row['Mem_Id']) {
+                  echo  "Serial No:   " . $count_book . ", ISBN:  " . " " . $row["Book_id"] . " " . ", Name: " . " " . $row["Book_Name"] . " " . ", Issue Date:   "  . " " . $row["Issue_date"] . " " . " , Return Date: " . " " . $row["Return_date"];
+                  echo "<br><br>";
+                  $count_book = $count_book + 1;
+               }
+            }
+         }
+
+
+         ?>
+
+      </ol>
+
+
 
    </div>
+
+   <a href="all_book_list_member.php">
+      <button class="SA">View All books</button>
+   </a>
 
    <div class="edit_borrowed">
 
       <div>
 
-         <form class="form_e" method = "post" style="width: 80%;">
+         <form class="form_e" method="post" style="width: 80%;">
             <h1 class="hi3"> Edit borrowed list</h1>
             <label>Enter the ISBN number:</label> <br>
-            <input type="text" name="book_isbn" >
+            <input type="text" name="book_isbn">
             <input class="SE" type="submit" name="delete" value="Delete">
          </form>
 
          <?php
          if (isset($_POST['delete'])) {
 
-             
-             $sql = "DELETE FROM borrowed_book_list WHERE Mem_Id = $_SESSION[user_id]  and Book_id = $_POST[book_isbn]";
-             $result = mysqli_query($connection, $sql);
 
-             ?>
-             <script type="text/javascript">
-                alert("Successfully Deleted! Please Refresh");
-             </script>
-          <?php
-            
+            $sql = "DELETE FROM borrowed_book_list WHERE Mem_Id = $_SESSION[user_id]  and Book_id = $_POST[book_isbn]";
+            $result = mysqli_query($connection, $sql);
+
+         ?>
+            <script type="text/javascript">
+               alert("Successfully Deleted! Please Refresh");
+            </script>
+         <?php
+
          }
          ?>
 
-         
+
 
       </div>
 
@@ -166,90 +168,86 @@ if (isset($_POST['logout'])) {
 
    <?php
 
-$count = 0;
+   $count = 0;
 
-if (isset($_POST['book_isbn'])) {
-   $sql = "SELECT Book_id, Book_name, Genre from `all_book_list`";
-   $res = mysqli_query($connection, $sql);
-   $bookname = null;
-   while ($row = mysqli_fetch_assoc($res)) {
-      if ($row['Book_id'] == $_POST['book_isbn']) {
-         $count = $count + 1;
-         $bookname = $row['Book_name'];
-         break;
+   if (isset($_POST['book_isbn'])) {
+      $sql = "SELECT Book_id, Book_name, Genre from `all_book_list`";
+      $res = mysqli_query($connection, $sql);
+      $bookname = null;
+      while ($row = mysqli_fetch_assoc($res)) {
+         if ($row['Book_id'] == $_POST['book_isbn']) {
+            $count = $count + 1;
+            $bookname = $row['Book_name'];
+            break;
+         }
       }
    }
-}
 
 
-if (isset($_POST['search'])) {
+   if (isset($_POST['search'])) {
 
-   if ($count == 1) {
-
-?>
-      <script type="text/javascript">
-         alert("Book Found!");
-      </script>
-   <?php
-   } else {
+      if ($count == 1) {
 
    ?>
-      <script type="text/javascript">
-         alert("Sorry, this Book is not available");
-      </script>
-   <?php
+         <script type="text/javascript">
+            alert("Book Found!");
+         </script>
+      <?php
+      } else {
 
+      ?>
+         <script type="text/javascript">
+            alert("Sorry, this Book is not available");
+         </script>
+         <?php
+
+      }
    }
-}
 
 
-if (isset($_POST['add_to_list'])) {
+   if (isset($_POST['add_to_list'])) {
 
-   $issue_date = strtotime("today");
-   $issue_date_store = date("Y-m-d h:i:s", $issue_date);
+      $issue_date = strtotime("today");
+      $issue_date_store = date("Y-m-d h:i:s", $issue_date);
 
-   $return_date = strtotime("+3 Month");
-   $return_date_store = date("Y-m-d H:i:s", $return_date);
-  
-   
+      $return_date = strtotime("+3 Month");
+      $return_date_store = date("Y-m-d H:i:s", $return_date);
 
-   if ($count == 1) {
 
-      if($count_book<=10){
-         mysqli_query($connection, "INSERT INTO `borrowed_book_list` VALUES ('$_SESSION[user_id]' , '$_POST[book_isbn]' , '$bookname', '$issue_date_store', '$return_date_store')");
-         
+
+      if ($count == 1) {
+
+         if ($count_book <= 10) {
+            mysqli_query($connection, "INSERT INTO `borrowed_book_list` VALUES ('$_SESSION[user_id]' , '$_POST[book_isbn]' , '$bookname', '$issue_date_store', '$return_date_store')");
+
+
+         ?>
+            <script type="text/javascript">
+               alert("Book Added!");
+            </script>
+         <?php
+         } else {
+
+         ?>
+            <script type="text/javascript">
+               alert("Sorry , You Can Not Add More Than 10 Books!");
+            </script>
+         <?php
+
+
+         }
+      } else {
 
          ?>
          <script type="text/javascript">
-            alert("Book Added!");
+            alert("Sorry, this Book is not available");
          </script>
-      <?php
-      }
-
-      else{
-         
-         ?>
-         <script type="text/javascript">
-            alert("Sorry , You Can Not Add More Than 10 Books!");
-         </script>
-      <?php
-
+   <?php
 
       }
-
-
-   } else {
+   }
 
    ?>
-      <script type="text/javascript">
-         alert("Sorry, this Book is not available");
-      </script>
-<?php
-
-   }
-}
-
-?>
 
 
 
